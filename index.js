59e586042fcd83783984451b71c2cd9a31d66c0d
marilynakinyioyoo <marilyn.oyoo@student.moringaschool.com>
2024-07-14 17:53:37 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution in this file!
+const employee = {
+    name:"John",
+    streetadress: "12 Main St."
+}
+updateEmployeeWithKeyAndValue(employee, "car model", "Camry")
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    let employees = {...employee}
+    employees[key] = value
+    return employees
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+
+
+function deleteFromEmployeeByKey(employee, key) {
+    return {
+        employee
+    }
+}
+//function destructivelyDeleteFromEmployeeByKey(employee){
+
+   //return {
+    //employee
+   //}
+
+//}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+
+}
+
+
+
+
